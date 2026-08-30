3539838cab490bc742feb6debf81432b744cafd2
Vadim Godunko <vgodunko@gmail.com>
2026-08-30 23:02:04 +0400

    Bind `esp_event_handler_instance_register` and related types.

--- source/espidf-event.adb
+++ source/espidf-event.adb
@@ -8,6 +8,26 @@ with ESPIDF.Ada_ESP_Check_Error;
 
 package body ESPIDF.Event is
 
+   -----------------------------------------
+   -- esp_event_handler_instance_register --
+   -----------------------------------------
+
+   procedure esp_event_handler_instance_register
+     (event_base        : esp_event_base_t;
+      event_id          : int32_t;
+      event_handler     : not null esp_event_handler_t;
+      event_handler_arg : System.Address;
+      instance          : out esp_event_handler_instance_t) is
+   begin
+      Ada_ESP_Check_Error
+        (esp_event_handler_instance_register
+          (event_base,
+           event_id,
+           event_handler,
+           event_handler_arg,
+           instance));
+   end esp_event_handler_instance_register;
+
    -----------------------------------
    -- esp_event_loop_create_default --
    -----------------------------------
--- source/espidf-event.ads
+++ source/espidf-event.ads
@@ -4,12 +4,47 @@
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 --
 
+with System;
+
+with ESPIDF.C_Strings;
+
 package ESPIDF.Event is
+
+   type esp_event_base_t is new ESPIDF.C_Strings.const_char_ptr;
+
+   type esp_event_handler_t is
+     access procedure
+       (event_handler_arg : System.Address;
+        event_base        : esp_event_base_t;
+        event_id          : int32_t;
+        event_data        : System.Address) with Convention => C;
+
+   type esp_event_handler_instance_t is private;
 
    function esp_event_loop_create_default return esp_err_t
      with Import, Convention => C,
           External_Name => "esp_event_loop_create_default";
 
    procedure esp_event_loop_create_default;
-   
+
+   function esp_event_handler_instance_register
+     (event_base        : esp_event_base_t;
+      event_id          : int32_t;
+      event_handler     : not null esp_event_handler_t;
+      event_handler_arg : System.Address;
+      instance          : out esp_event_handler_instance_t) return esp_err_t
+     with Import, Convention => C,
+          External_Name => "esp_event_handler_instance_register";
+
+   procedure esp_event_handler_instance_register
+     (event_base        : esp_event_base_t;
+      event_id          : int32_t;
+      event_handler     : not null esp_event_handler_t;
+      event_handler_arg : System.Address;
+      instance          : out esp_event_handler_instance_t);
+
+private
+
+   type esp_event_handler_instance_t is new System.Address;
+
 end ESPIDF.Event;
